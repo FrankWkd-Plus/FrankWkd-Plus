67613f58faf1366ad47755795dc048496c70b7e2
OutwardlyVerse <227823526+FrankWkd-Plus@users.noreply.github.com>
2026-01-14 16:21:23 +0800

    refactor: optimize logger_init and update configurations

---
+++ api_client_v11_21.go
@@ -0,0 +1,3 @@
+# Update Metadata
+# Timestamp: 2026-01-14 16:21:23
+# Token: 716e8cb4-65d5-48ad-8eb7-7510452225be
---
+++ logger_init_v11_21.go
@@ -0,0 +1,3 @@
+# Update Metadata
+# Timestamp: 2026-01-14 16:21:23
+# Token: d3a03ac4-8a7f-4a32-9fcb-b3491e61ad19
