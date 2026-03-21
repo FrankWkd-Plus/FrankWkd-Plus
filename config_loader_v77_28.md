a21b1227010295228ecdb05374cb182ebc698e6a
OutwardlyVerse <227823526+FrankWkd-Plus@users.noreply.github.com>
2026-03-21 15:28:22 +0800

    refactor: optimize config_loader and update configurations

---
+++ config_loader_v77_28.md
@@ -0,0 +1,3 @@
+# Update Metadata
+# Timestamp: 2026-03-21 15:28:22
+# Token: bb2cff85-f5bf-4166-91e9-05de6a39b78a
---
+++ logger_init_v77_28.go
@@ -0,0 +1,3 @@
+# Update Metadata
+# Timestamp: 2026-03-21 15:28:22
+# Token: 201156cc-54d0-4b35-a11b-4290e03ff696
